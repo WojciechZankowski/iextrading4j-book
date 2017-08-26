3f9eea79276fba91b466c99a198c0ab0f7f9b719
WojciechZankowski <info@zankowski.pl>
2017-08-26 15:51:30 +0200

    Order Book impl

---
+++ iextrading4j-book-sample/src/main/java/pl/zankowski/iextrading4j/book/sample/OrderBookSample.java
@@ -0,0 +1,61 @@
+package pl.zankowski.iextrading4j.book.sample;
+
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PacketListener;
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.Pcaps;
+import org.pcap4j.packet.Packet;
+import pl.zankowski.iextrading4j.book.IEXOrderBook;
+import pl.zankowski.iextrading4j.hist.api.message.IEXMessage;
+import pl.zankowski.iextrading4j.hist.api.message.IEXSegment;
+import pl.zankowski.iextrading4j.hist.deep.IEXDEEPMessageBlock;
+import pl.zankowski.iextrading4j.hist.deep.trading.message.IEXPriceLevelUpdateMessage;
+
+import java.io.IOException;
+
+/**
+ * @author Wojciech Zankowski
+ */
+public class OrderBookSample {
+
+    private final OrderBookStore orderBookStore = new OrderBookStore();
+
+    public static void main(String[] args) throws IOException, PcapNativeException, InterruptedException, NotOpenException {
+        OrderBookSample orderBookSample = new OrderBookSample();
+        orderBookSample.readDEEPSample();
+    }
+
+    private void readDEEPSample() throws PcapNativeException, NotOpenException, InterruptedException {
+        PcapHandle handle;
+        try {
+            handle = Pcaps.openOffline("F:\\IEXTrading\\20170823_IEXTP1_DEEP1.0.pcap", PcapHandle.TimestampPrecision.NANO);
+        } catch (PcapNativeException e) {
+            handle = Pcaps.openOffline("F:\\IEXTrading\\20170823_IEXTP1_DEEP1.0.pcap");
+        }
+
+        handle.loop(-1, new PacketListener() {
+            @Override
+            public void gotPacket(Packet packet) {
+                byte[] data = packet.getPayload().getPayload().getPayload().getRawData();
+                IEXSegment block = IEXDEEPMessageBlock.createIEXSegment(data);
+                for (IEXMessage iexMessage : block.getMessages()) {
+                    if (iexMessage instanceof IEXPriceLevelUpdateMessage) {
+                        IEXPriceLevelUpdateMessage iexPriceLevelUpdateMessage = (IEXPriceLevelUpdateMessage) iexMessage;
+                        IEXOrderBook iexOrderBook = orderBookStore.getOrderBook(iexPriceLevelUpdateMessage.getSymbol())
+                                .orElseGet(() -> new IEXOrderBook(iexPriceLevelUpdateMessage.getSymbol()));
+                        iexOrderBook.priceLevelUpdate(iexPriceLevelUpdateMessage);
+                        orderBookStore.updateOrderBook(iexOrderBook);
+                        if (iexOrderBook.getSymbol().equals("AAPL")) {
+                            System.out.println(iexOrderBook);
+                        }
+                    }
+                }
+            }
+        });
+
+
+        handle.close();
+    }
+
+}
---
+++ iextrading4j-book-sample/src/main/java/pl/zankowski/iextrading4j/book/sample/OrderBookStore.java
@@ -0,0 +1,24 @@
+package pl.zankowski.iextrading4j.book.sample;
+
+import pl.zankowski.iextrading4j.book.IEXOrderBook;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author Wojciech Zankowski
+ */
+public class OrderBookStore {
+
+    private final Map<String, IEXOrderBook> orderBookMap = new HashMap<>();
+
+    public Optional<IEXOrderBook> getOrderBook(String symbol) {
+        return Optional.ofNullable(orderBookMap.get(symbol));
+    }
+
+    public void updateOrderBook(IEXOrderBook iexOrderBook) {
+        orderBookMap.put(iexOrderBook.getSymbol(), iexOrderBook);
+    }
+
+}
